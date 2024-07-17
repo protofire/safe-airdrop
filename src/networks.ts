221a70f71376c2f4164d1ec26374cd1b12e39834
goxibyte <denissmolonski@gmail.com>
2024-07-17 12:23:42 +0200

    Staging (#7)
    * feat: add astar networks
    
    * feat: add support for Zircuit Network
    
    * feat: add mantle
    
    * feat: add kroma
    
    * Merge branch 'staging' into production
    
    * feat: add harmony
    
    * feat: add linea
    
    ---------
    
    Co-authored-by: msvstj <savastijonok@gmail.com>
    Co-authored-by: Leoni Mella <leoni.mella@gmail.com>
    Co-authored-by: Den Smalonski <dzianis.smalonski@protofire.io>

--- src/networks.ts
+++ src/networks.ts
@@ -492,4 +492,59 @@ export const networkInfo = new Map<number, NetworkInfo>([
       stagingBaseAPI: "https://transaction.staging.safe.kroma.network/api/v1",
     },
   ],
+  [
+    1666600000,
+    {
+      chainID: 1666600000,
+      name: "Harmony",
+      shortName: "hmy-s0",
+      currencySymbol: "ONE",
+      baseAPI: "https://transaction.multisig.harmony.one/api/v1",
+      stagingBaseAPI: "https://transaction.staging-safe.harmony.one/api/v1",
+    },
+  ],
+  [
+    1666700000,
+    {
+      chainID: 1666700000,
+      name: "Harmony Testnet",
+      shortName: "hmy-b-s0",
+      currencySymbol: "ONE",
+      baseAPI: "https://transaction-testnet.multisig.harmony.one/api/v1",
+      stagingBaseAPI: "https://transaction-testnet.staging-safe.harmony.one/api/v1",
+    },
+  ],
+  [
+    59144,
+    {
+      chainID: 59144,
+      name: "Linea",
+      shortName: "linea",
+      currencySymbol: "ETH",
+      baseAPI: "https://transaction.safe.linea.build/api/v1",
+      stagingBaseAPI: "https://transaction.staging.safe.linea.build/api/v1",
+    },
+  ],
+  [
+    59140,
+    {
+      chainID: 59140,
+      name: "Linea Goerli",
+      shortName: "linea-testnet",
+      currencySymbol: "ETH",
+      baseAPI: "https://transaction-testnet.safe.linea.build/api/v1",
+      stagingBaseAPI: "https://transaction-testnet.staging.safe.linea.build/api/v1",
+    },
+  ],
+  [
+    59141,
+    {
+      chainID: 59141,
+      name: "Linea Sepolia",
+      shortName: "linea-sepolia",
+      currencySymbol: "ETH",
+      baseAPI: "https://transaction-sepolia.staging.safe.linea.build/api/v1",
+      stagingBaseAPI: "https://transaction-sepolia.staging.safe.linea.build/api/v1",
+    },
+  ],
 ]);
