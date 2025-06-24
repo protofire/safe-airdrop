1143d32f4d1ec66969e4c4ca8e08c5f77047bdad
John Pochta <johnpochta1@gmail.com>
2025-06-24 11:35:54 +0200

    feat(add-networks): added game7 and gravity (#20)
    * feat(add-networks): added game7 and gravity
    
    * feat(add-networks): added game7 and gravity

--- src/networks.ts
+++ src/networks.ts
@@ -799,4 +799,37 @@ export const networkInfo = new Map<number, NetworkInfo>([
       stagingBaseAPI: "https://transaction.staging.safe.berachain.com/api/v1",
     },
   ],
+  [
+    2187,
+    {
+      chainID: 2187,
+      name: "Game7",
+      shortName: "g7",
+      currencySymbol: "G7",
+      baseAPI: "https://transaction.safe.game7.io/api/v1",
+      stagingBaseAPI: "https://transaction.staging.safe.game7.io/api/v1",
+    },
+  ],
+  [
+    1625,
+    {
+      chainID: 1625,
+      name: "Gravity Alpha Mainnet",
+      shortName: "gravity",
+      currencySymbol: "G",
+      baseAPI: "https://transaction.safe.gravity.xyz/api/v1",
+      stagingBaseAPI: "https://transaction.staging.safe.gravity.xyz/api/v1",
+    },
+  ],
+  [
+    13746,
+    {
+      chainID: 13746,
+      name: "Game7 Testnet",
+      shortName: "g7t",
+      currencySymbol: "TG7T",
+      baseAPI: "https://transaction-testnet.staging.safe.game7.io/api/v1",
+      stagingBaseAPI: "https://transaction-testnet.staging.safe.game7.io/api/v1",
+    },
+  ],
 ]);
