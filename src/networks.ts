d06d65401d0ffe3a28b028f0adba4d5bb8bab351
John Pochta <johnpochta1@gmail.com>
2025-07-07 11:39:23 +0200

    feat(add-shape-network): added shape network (#24) (#25)

--- src/networks.ts
+++ src/networks.ts
@@ -843,4 +843,26 @@ export const networkInfo = new Map<number, NetworkInfo>([
       stagingBaseAPI: "https://transaction-testnet.staging.safe.gravity.xyz/api/v1",
     },
   ],
+  [
+    360,
+    {
+      chainID: 360,
+      name: "Shape",
+      shortName: "shape",
+      currencySymbol: "ETH",
+      baseAPI: "https://transaction.safe.shape.network/api/v1",
+      stagingBaseAPI: "https://transaction.staging.safe.shape.network/api/v1",
+    },
+  ],
+  [
+    11011,
+    {
+      chainID: 11011,
+      name: "Shape Sepolia Testnet",
+      shortName: "shapesep",
+      currencySymbol: "ETH",
+      baseAPI: "https://transaction-testnet.safe.shape.network/api/v1",
+      stagingBaseAPI: "https://transaction-testnet.staging.safe.shape.network/api/v1",
+    },
+  ],
 ]);
